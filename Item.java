25e9dee1aaf9be6bd53426948b1fdbcd3bb3006b
Kotturukeerthana <103315102+Kotturukeerthana@users.noreply.github.com>
2023-07-18 14:59:07 +0000

    adding Item file code changes

---
+++ Item.java
@@ -0,0 +1,41 @@
+class Item {
+	private String productName;
+	private int quantity;
+	private double unitPrice;
+
+	/*
+	 * 1. Implement the constructor
+	 */
+	Item(String productName, int quantity, double unitPrice) {
+		this.productName = productName;
+		this.quantity = quantity;
+		this.unitPrice = unitPrice;
+	}
+
+	public String toString() {
+		String s = this.productName + ":";
+		s = s + this.quantity + "\n";
+		return s;
+	}
+
+	/*
+	 * 2. Implement this method
+	 */
+	public String getProductName() {
+		return productName;
+	}
+
+	/*
+	 * 3. Implement this method
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/*
+	 * 4. Implement this method
+	 */
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+}
